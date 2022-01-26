6d1d904a4277748e94bab645672041fe665086f5
jacob gishwiller <sirbruce13@yahoo.com>
2022-01-26 11:09:07 -0700

    finished, added comment

--- Output/Program.cs
+++ Output/Program.cs
@@ -6,13 +6,13 @@ namespace Output
    {
       static void Main(string[] args)
       {
-         Console.WriteLine("1 2 3 4");
-         Console.WriteLine($"{1} {2} {3} {4}");
+         Console.WriteLine("1 2 3 4"); // print line one
+         Console.WriteLine($"{1} {2} {3} {4}"); //print line two
          Console.Write("1 ");
          Console.Write("2 ");
          Console.Write("3 ");
          Console.Write("4");
-        
+        // print line three
 
       }
    }
